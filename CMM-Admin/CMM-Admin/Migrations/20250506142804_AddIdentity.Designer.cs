4a1fda70886bbb12628a33e733bffe42e8b74e78
addledo <jontyfpine@gmail.com>
2025-05-06 15:52:15 +0100

    Add identity to DB

--- CMM-Admin/CMM-Admin/Migrations/20250506142804_AddIdentity.Designer.cs
+++ CMM-Admin/CMM-Admin/Migrations/20250506142804_AddIdentity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CMM_Admin.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CMM_Admin.Migrations
 {
     [DbContext(typeof(MuseumContext))]
-    partial class MuseumContextModelSnapshot : ModelSnapshot
+    [Migration("20250506142804_AddIdentity")]
+    partial class AddIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.15");
