8f9956f7f36db1e24b24dc8f80bfeca5c0586af4
addledo <jontyfpine@gmail.com>
2025-05-12 20:44:00 +0100

    Add artifact qr code

--- CMM-Admin/CMM-Admin/Pages/Artifacts/Details.cshtml.cs
+++ CMM-Admin/CMM-Admin/Pages/Artifacts/Details.cshtml.cs
@@ -1,18 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using CMM_Admin.Data;
 using CMM_Admin.Data.Models;
+using QRCoder;
 
 namespace CMM_Admin.Pages.Artifacts
 {
     public class DetailsModel(MuseumContext context) : PageModel
     {
         public Artifact Artifact { get; set; } = null!;
+        public string? QrCodeBase64 { get; set; }
 
         public async Task<IActionResult> OnGetAsync(string? id)
         {
@@ -30,6 +28,15 @@ namespace CMM_Admin.Pages.Artifacts
             {
                 Artifact = artifact;
             }
+            
+            
+            using var qrGenerator = new QRCodeGenerator();
+            using var qrData = qrGenerator.CreateQrCode(Artifact.ArtifactId, QRCodeGenerator.ECCLevel.M);
+            using var qrCode = new PngByteQRCode(qrData);
+
+            var qrCodeBytes = qrCode.GetGraphic(20);
+            QrCodeBase64 = Convert.ToBase64String(qrCodeBytes);
+            
             return Page();
         }
     }
