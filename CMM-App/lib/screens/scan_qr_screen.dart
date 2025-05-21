40bf1a9fbbc18965018aaf6f2adb769ad665d775
addledo <jontyfpine@gmail.com>
2025-05-21 14:15:52 +0100

    Refactor QrScreen / use custom app bar

--- CMM-App/lib/screens/scan_qr_screen.dart
+++ CMM-App/lib/screens/scan_qr_screen.dart
@@ -1,10 +1,9 @@
+import 'package:cheshire_military_museum_tour/widgets/app_bar.dart';
 import 'package:cheshire_military_museum_tour/widgets/bottom_info_card.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
-import 'package:cheshire_military_museum_tour/utils/responsive_utils.dart';
 import 'package:cheshire_military_museum_tour/screens/artefact_detail_screen.dart';
 import '../models/medal_tracker.dart';
-import '../widgets/circle_button.dart';
 import '../widgets/navigation_bar.dart';
 
 class ScanQRScreen extends StatefulWidget {
@@ -35,34 +34,14 @@ class _ScanQRScreenState extends State<ScanQRScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final resp = ResponsiveUtils.instance;
-
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        toolbarHeight: 100,
-        backgroundColor: Colors.white,
-        elevation: 0,
-        centerTitle: true,
-        // Center the title
+      appBar: CustomAppBar(
+        context: context,
         title: Text(
           'Scan QR',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: resp.fontSize(24),
-            fontFamily: 'Inter',
-            fontWeight: FontWeight.w700,
-          ),
+          style: TextStyle(color: Colors.black, fontSize: 24, fontFamily: 'Inter', fontWeight: FontWeight.w700),
         ),
-        leading: Padding(
-          padding: EdgeInsets.only(left: resp.getHorizontalSpacing(8)),
-          child: CircleButton(
-            icon: Icons.arrow_back,
-            onPressed: () => Navigator.pop(context),
-            backgroundColor: Theme.of(context).primaryColor,
-          ),
-        ),
-        leadingWidth: resp.scaleWidth(64),
       ),
       body: SafeArea(
         bottom: false,
@@ -72,14 +51,14 @@ class _ScanQRScreenState extends State<ScanQRScreen> {
             Expanded(
               flex: 3,
               child: Padding(
-                padding: EdgeInsets.all(resp.getHorizontalSpacing(65)),
+                padding: EdgeInsets.all(40),
                 child: Center(
                   child: AspectRatio(
                     aspectRatio: 1.0,
                     child: Container(
                       decoration: BoxDecoration(
                         color: Colors.white,
-                        borderRadius: resp.getBorderRadius(12),
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
                         boxShadow: [
                           BoxShadow(
                             color: Colors.black.withValues(alpha: 0.6),
