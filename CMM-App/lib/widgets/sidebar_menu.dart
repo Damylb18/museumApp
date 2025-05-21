4d0ec6c323552a2ab523a4a4d172f66a5e7e5a9b
addledo <jontyfpine@gmail.com>
2025-05-21 16:22:42 +0100

    Sidebar page formatting

--- CMM-App/lib/widgets/sidebar_menu.dart
+++ CMM-App/lib/widgets/sidebar_menu.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import '../utils/navigation_utils.dart';
@@ -20,10 +18,7 @@ class SideBarMenu extends StatelessWidget {
           height: MediaQuery.of(context).size.height,
           decoration: const BoxDecoration(
             color: Colors.black87,
-            borderRadius: BorderRadius.only(
-              topRight: Radius.circular(30),
-              bottomRight: Radius.circular(30),
-            ),
+            borderRadius: BorderRadius.only(topRight: Radius.circular(30), bottomRight: Radius.circular(30)),
           ),
           padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 24),
           child: Column(
@@ -45,11 +40,7 @@ class SideBarMenu extends StatelessWidget {
                 leading: const Icon(Icons.phone, color: Colors.white),
                 title: const Text('Contact us', style: TextStyle(color: Colors.white)),
                 onTap: () {
-                  NavigationUtils.openWebPage(
-                    context,
-                    'Contact Us',
-                    'https://cheshiremilitarymuseum.org.uk/contact/',
-                  );
+                  NavigationUtils.openWebPage(context, 'Contact Us', 'https://cheshiremilitarymuseum.org.uk/contact/');
                 },
               ),
               // "Privacy Policy" link
@@ -71,41 +62,26 @@ class SideBarMenu extends StatelessWidget {
                 children: [
                   GestureDetector(
                     onTap: () {
-                      UrlUtils.openExternalUrl(
-                        context,
-                        'https://www.facebook.com/cheshiremilitarymuseum',
-                      );
+                      UrlUtils.openExternalUrl(context, 'https://www.facebook.com/cheshiremilitarymuseum');
                     },
                     child: Container(
                       width: 40,
                       height: 40,
-                      decoration: BoxDecoration(
-                        color: Colors.blue[800],
-                        shape: BoxShape.circle,
-                      ),
+                      decoration: BoxDecoration(color: Colors.blue[800], shape: BoxShape.circle),
                       child: const Icon(Icons.facebook, color: Colors.white, size: 24),
                     ),
                   ),
                   const SizedBox(width: 16),
                   GestureDetector(
                     onTap: () {
-                      UrlUtils.openExternalUrl(
-                        context,
-                        'https://www.instagram.com/cheshiremilitarymuseum',
-                      );
+                      UrlUtils.openExternalUrl(context, 'https://www.instagram.com/cheshiremilitarymuseum');
                     },
                     child: Container(
                       width: 40,
                       height: 40,
-                      decoration: const BoxDecoration(
-                        color: Colors.pinkAccent,
-                        shape: BoxShape.circle,
-                      ),
+                      decoration: const BoxDecoration(color: Colors.pinkAccent, shape: BoxShape.circle),
                       padding: const EdgeInsets.all(8),
-                      child: SvgPicture.asset(
-                        'assets/icons/Instagram.svg',
-                        fit: BoxFit.contain,
-                      ),
+                      child: SvgPicture.asset('assets/icons/Instagram.svg', fit: BoxFit.contain),
                     ),
                   ),
                   const SizedBox(width: 16),
@@ -119,17 +95,11 @@ class SideBarMenu extends StatelessWidget {
                     icon: Container(
                       width: 40,
                       height: 40,
-                      decoration: const BoxDecoration(
-                        color: Color(0xFF00AF87),
-                        shape: BoxShape.circle,
-                      ),
+                      decoration: const BoxDecoration(color: Color(0xFF00AF87), shape: BoxShape.circle),
                       child: Center(
                         child: Padding(
                           padding: EdgeInsets.all(6.0),
-                          child: SvgPicture.asset(
-                            'assets/icons/tripadvisor.svg',
-                            fit: BoxFit.contain,
-                          ),
+                          child: SvgPicture.asset('assets/icons/tripadvisor.svg', fit: BoxFit.contain),
                         ),
                       ),
                     ),
