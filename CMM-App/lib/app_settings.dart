d961ae1194693bc302a9e421c85991c3e03f19bc
addledo <jontyfpine@gmail.com>
2025-05-22 08:09:20 +0100

    Move API base URL into AppSettings file

---
+++ CMM-App/lib/app_settings.dart
@@ -0,0 +1,5 @@
+class AppSettings {
+  /// The base URL for the api connection. Change this to the server the admin panel is deployed on.
+  /// Format: "https://\<SERVER\>/cmm/api"
+  static const String apiBaseUrl = 'https://2326780.win.studentwebserver.co.uk/cmm/api';
+}
