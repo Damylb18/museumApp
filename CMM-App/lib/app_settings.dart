9ea23e7a7c78519e04b0ef7de7cbe8180edd477a
addledo <jontyfpine@gmail.com>
2025-05-22 08:28:32 +0100

    Extract social media links to app settings file

--- CMM-App/lib/app_settings.dart
+++ CMM-App/lib/app_settings.dart
@@ -3,3 +3,16 @@ class AppSettings {
   /// Format: "https://\<SERVER\>/cmm/api"
   static const String apiBaseUrl = 'https://2326780.win.studentwebserver.co.uk/cmm/api';
 }
+
+/// Links to locations on the CMM website, to be opened from within the app.
+class WebsiteLinks {
+  static const String contactUs = 'https://cheshiremilitarymuseum.org.uk/contact/';
+  static const String privacyPolicy = 'https://cheshiremilitarymuseum.org.uk/privacy-policy/';
+}
+
+/// Links to the CMM social medias.
+class SocialMediaLinks {
+  static const String facebook = 'https://www.facebook.com/cheshiremilitarymuseum';
+  static const String instagram = 'https://www.instagram.com/cheshiremilitarymuseum';
+  static const String tripadvisor = 'https://www.tripadvisor.co.uk/Attraction_Review-g186233-d3589569-Reviews-Cheshire_Military_Museum-Chester_Cheshire_England.html';
+}
--- CMM-App/lib/widgets/sidebar_menu.dart
+++ CMM-App/lib/widgets/sidebar_menu.dart
@@ -1,3 +1,4 @@
+import 'package:cheshire_military_museum_tour/app_settings.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import '../utils/navigation_utils.dart';
@@ -35,15 +36,17 @@ class SideBarMenu extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: 10),
-              // "Contact Us" link
+
+              // CONTACT US
               ListTile(
                 leading: const Icon(Icons.phone, color: Colors.white),
                 title: const Text('Contact us', style: TextStyle(color: Colors.white)),
                 onTap: () {
-                  NavigationUtils.openWebPage(context, 'Contact Us', 'https://cheshiremilitarymuseum.org.uk/contact/');
+                  NavigationUtils.openWebPage(context, 'Contact Us', WebsiteLinks.contactUs);
                 },
               ),
-              // "Privacy Policy" link
+
+              // PRIVACY POLICY
               ListTile(
                 leading: const Icon(Icons.lock, color: Colors.white),
                 title: const Text('Privacy Policy', style: TextStyle(color: Colors.white)),
@@ -51,7 +54,7 @@ class SideBarMenu extends StatelessWidget {
                   NavigationUtils.openWebPage(
                     context,
                     'Privacy Policy',
-                    'https://cheshiremilitarymuseum.org.uk/privacy-policy/',
+                    WebsiteLinks.privacyPolicy,
                   );
                 },
               ),
@@ -60,9 +63,10 @@ class SideBarMenu extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+                  // FACEBOOK
                   GestureDetector(
                     onTap: () {
-                      UrlUtils.openExternalUrl(context, 'https://www.facebook.com/cheshiremilitarymuseum');
+                      UrlUtils.openExternalUrl(context, SocialMediaLinks.facebook);
                     },
                     child: Container(
                       width: 40,
@@ -72,9 +76,11 @@ class SideBarMenu extends StatelessWidget {
                     ),
                   ),
                   const SizedBox(width: 16),
+
+                  // INSTAGRAM
                   GestureDetector(
                     onTap: () {
-                      UrlUtils.openExternalUrl(context, 'https://www.instagram.com/cheshiremilitarymuseum');
+                      UrlUtils.openExternalUrl(context, SocialMediaLinks.instagram);
                     },
                     child: Container(
                       width: 40,
@@ -84,12 +90,14 @@ class SideBarMenu extends StatelessWidget {
                       child: SvgPicture.asset('assets/icons/Instagram.svg', fit: BoxFit.contain),
                     ),
                   ),
+
+                  // TRIPADVISOR
                   const SizedBox(width: 16),
                   IconButton(
                     onPressed: () {
                       UrlUtils.openExternalUrl(
                         context,
-                        'https://www.tripadvisor.com/Attraction_Review-g186390-d212257-Reviews-Cheshire_Military_Museum-Chester_Cheshire_England.html',
+                        SocialMediaLinks.tripadvisor,
                       );
                     },
                     icon: Container(
